e5d08ce5963677b61cfd513b575213ea15dcb3f2
ReverchonLucas <lucas.reverchon@ecoles-epsi.net>
2024-12-20 16:26:37 +0100

    projet

---
+++ src/main/java/fr/epsi/phototeque/PhototequeApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.phototeque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhototequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhototequeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/epsi/phototeque/PhototequeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi.phototeque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhototequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
